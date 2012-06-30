fa0c7f1807a5274d755cad7ee7c4a6a28695cf69
Ruoran Liu <rrliu@thoughtworks.com>
2012-06-30 16:16:46 +0530

    Ruoran: Init empty framework of Ruby on Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AngryRickshaw::Application.config.secret_token = '01f4346b2a06f1c0832b8bceca39040fd50830dd9bf7c52b8bf9b947cd819536476d331ed349e728b310f75757ac83cff94c9ca1c287bcac7fdf60e958420d9b'
