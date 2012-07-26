6b21e11b37a6ff060ce56d0b6210d4c2efc2e3aa
erlanggat <etjhie@thoughtworks.com>
2012-07-26 16:57:28 +1000

    [Erlangga/Greg] change the menu bar image

--- app/assets/javascripts/menu.js
+++ app/assets/javascripts/menu.js
@@ -1,5 +1,24 @@
 $(document).ready(function () {
     $(".menuBar").bind("mouseover", function(){
-        alert($(this).src);
+        changeMenuImageWithExtension($(this)[0], "_up.svg");
+    });
+
+    $(".menuBar").bind("mousedown", function(){
+        changeMenuImageWithExtension($(this)[0], "_dn.svg");
+    });
+
+    $(".menuBar").bind("mouseup", function(){
+        changeMenuImageWithExtension($(this)[0], "_up.svg");
+    });
+
+    $(".menuBar").bind("mouseout", function(){
+        changeMenuImageWithExtension($(this)[0], ".svg");
     });
 });
+
+function changeMenuImageWithExtension(menuItem, extension) {
+    var menuText = menuItem.src.split("/");
+    var folderName = menuText[menuText.length-2];
+
+    menuItem.src = "/assets/menubar/" + folderName + "/" + folderName + extension;
+}
