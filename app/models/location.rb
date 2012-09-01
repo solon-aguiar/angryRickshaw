b3fae36d069a2f9348cee1bae22fe3386733d08d
Junior Ales <edilson.ales.jr@gmail.com>
2012-08-31 21:44:32 -0300

    [Jr] Creating the html structure to Map section

--- app/models/location.rb
+++
@@ -1,6 +0,0 @@
-class Location < ActiveRecord::Base
-  attr_accessible :address, :latitude, :longitude
-
-  geocoded_by :address
-  after_validation :geocode, :if => :address_changed?
-end
--- spec/requests/currencies_spec.rb
+++ spec/requests/currencies_spec.rb
@@ -6,18 +6,18 @@ describe "Currency Page" do
 
 		it "should have the currency tables" do
 			visit '/currency'
-			page.should have_css('table#otherToRupees')
-			page.should have_css('table#rupeesToOther')
+			page.should have_selector('table#otherToRupees')
+			page.should have_selector('table#rupeesToOther')
 		end
 
 		it "should have the field where note is shown" do
 			visit '/currency'
-			page.should have_css('div', :class => 'custom-note')
+			page.should have_selector('div.custom-note')
 		end
 
 		it "should have the text field to insert the value" do
 			visit '/currency'
-			page.should have_css('input#amount')
+			page.should have_selector('input#amount')
 		end
 	end
 end
