6abfd112d50f5d6bfb8c2f9bf14ca7438050424a
Junior Ales <edilson.ales.jr@gmail.com>
2012-09-12 12:47:58 -0300

    [Jr] Adding validation in location model

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,8 +1,8 @@
 class Location < ActiveRecord::Base
 	attr_accessible :address, :category, :description, :latitude, :longitude, :name, :picture
 
-	#TODO create the test before
-	#validates :name, presence: true
-	#validates :longitude, presence: true
-	#validates :latitude, presence: true
+	validates :name, presence: true
+	validates :longitude, :numericality => { :greater_than => -180, :less_than_or_equal_to => 180 }, presence: true
+	validates :latitude, :numericality => { :greater_than => -90, :less_than_or_equal_to => 90 }, presence: true
+
 end
