9ddb6678b8156d8ff4b2f252a683db07bfcf8f79
Junior Ales <edilson.ales.jr@gmail.com>
2012-09-17 19:25:44 -0300

    [Jr] Fixing a duplicate command in migrations

--- db/migrate/20120917190103_removing_columns_from_activity.rb
+++ db/migrate/20120917190103_removing_columns_from_activity.rb
@@ -4,15 +4,9 @@ class RemovingColumnsFromActivity < ActiveRecord::Migration
 		remove_column :activities, :description
 		remove_column :activities, :location
 		remove_column :activities, :rating
-
-		add_column :activities, :location_id, :integer
-
 	end
 
 	def down
-
-		remove_column :activities, :location_id
-
 		add_column :activities, :category, :string
 		add_column :activities, :description, :string
 		add_column :activities, :location, :string
