e3341067c901a449dd46b64cadd29ca5fb49cf81
Junior Ales <edilson.ales.jr@gmail.com>
2012-09-20 22:41:46 -0300

    [Jr] Changing the way to test with fake data. Now using fixtures

--- spec/requests/home_spec.rb
+++ spec/requests/home_spec.rb
@@ -2,31 +2,40 @@ require 'spec_helper'
 
 describe "Home page" do
 
+	subject { page }
+
 	describe "have a menu" do
-		it "should show all links" do
-			visit root_path
-			page.should have_selector('a.thumbnail h2', :text => 'Map')
-			page.should have_selector('a.thumbnail h2', :text => 'Activities')
-			page.should have_selector('a.thumbnail h2', :text => 'About Us')
-			page.should have_selector('a.thumbnail h2', :text => 'Currency')
+		before { visit root_path }
+
+		describe "should show all links" do
+			it { should have_selector('a.thumbnail h2', :text => 'Map') }
+			it { should have_selector('a.thumbnail h2', :text => 'Activities') }
+			it { should have_selector('a.thumbnail h2', :text => 'About Us') }
+			it { should have_selector('a.thumbnail h2', :text => 'Currency') }
 		end
 
-		it "all links should work properly" do
-			visit root_path
-			click_link "Angry Rickshaw"
-			page.should have_selector('h1', :text => 'Angry Rickshaw')
-			visit root_path
-			click_link "Map"
-			page.should have_selector('li.span12 section h2', :text => 'Map')
-			visit root_path
-			click_link "Activities"
-			page.should have_selector('li.span12 section h2', :text => 'Activities')
-			visit root_path
-			click_link "About Us"
-			page.should have_selector('li.span12 section h2', :text => 'About Us')
-			visit root_path
-			click_link "Currency"
-			page.should have_selector('li.span12 section h2', :text => 'Currency')
+
+		describe "all links should work properly" do
+			it do
+				click_link "Angry Rickshaw"
+				should have_selector('h1', :text => 'Angry Rickshaw')
+			end
+			it do
+				click_link "Map"
+				should have_selector('li.span12 section h2', :text => 'Map')
+			end
+			it do
+				click_link "About Us"
+				should have_selector('li.span12 section h2', :text => 'About Us')
+			end
+			it do
+				click_link "Currency"
+				should have_selector('li.span12 section h2', :text => 'Currency')
+			end
+			it do
+				click_link "Activities"
+				should have_selector('li.span12 section h2', :text => 'Activities')
+			end
 		end
 	end
 end
