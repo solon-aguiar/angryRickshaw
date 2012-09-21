8110124ee0830e347900280606e6f078bbd1b32f
Junior Ales <edilson.ales.jr@gmail.com>
2012-09-20 21:07:36 -0300

    [Jr] Implementing Activity Description story - done

--- spec/requests/activities_spec.rb
+++ spec/requests/activities_spec.rb
@@ -1,43 +1,54 @@
 require 'spec_helper'
 
+def first_activity_shown
+	first_category_id = 1
+	location = Location.find_by_category_id(first_category_id)
+	Activity.find_by_location_id(location)
+end
+
 describe Activity do
 
 	include ActivityHelper
 
 	subject { page }
 
-	describe "should have the main structure" do
+	describe "Activity List page" do
 		before { visit all_activity_path }
-		it "has main title" do
-			should have_selector('h1', :text => 'Angry Rickshaw')
-		end
 
-		it "has the subtitle" do
-			should have_selector('h2#activityPageTitle', :text => 'Activities')
-		end
-
-		describe "each activity" do
-			it "has a link redirecting to google maps" do
-				should have_selector('a.btn', :text => 'Get Directions')
+		describe "should have the main structure" do
+			it "has main title" do
+				should have_selector('h1', :text => 'Angry Rickshaw')
 			end
-			it "has a link to get more activity details" do
-				should have_selector('a.btn', :text => 'More Details')
+
+			it "has the subtitle" do
+				should have_selector('h2#activityPageTitle', :text => 'Activities')
+			end
+
+			describe "each activity" do
+				it "has a link redirecting to google maps" do
+					should have_selector('a.btn', :text => 'Get Directions')
+				end
+				it "has a link to get more activity details" do
+					should have_selector('a.btn', :text => 'More Details')
+				end
+				it "it has the short description being shown" do
+					activity = first_activity_shown()
+					should have_selector('div.location-text p', :text => short_description(activity.location.description))
+				end
+			end
+		end
+
+		describe "links should point the right places" do
+			activity = first_activity_shown()
+
+			it "has the correct google maps link" do
+				find_link("Get Directions")[:href].should == gmaps_url(activity.location)
+			end
+
+			it "has the correct details link" do
+				find_link("More Details")[:href].should == activity_path(activity)
 			end
 		end
 	end
 
-	describe "links point the right places" do
-		FIRST_CATEGORY_ID = 1
-		location = Location.find_by_category_id(FIRST_CATEGORY_ID)
-		activity = Activity.find_by_location_id(location)
-		before { visit all_activity_path }
-
-		it "has the correct google maps link" do
-			find_link("Get Directions")[:href].should == gmaps_url(activity.location)
-		end
-
-		it "has the correct details link" do
-			find_link("More Details")[:href].should == activity_path(activity)
-		end
-	end
 end
--- test/unit/activity_helper_test.rb
+++ test/unit/activity_helper_test.rb
@@ -15,13 +15,28 @@ class ActivityHelperTest < ActiveSupport::TestCase
 
 	test "google maps generated url" do
 		expected_url = "http://maps.google.com/maps?q=Test+Location,+Test+Address&ll=1.0,2.0"
-		location = FactoryGirl.create(:location)
-
-		assert_equal expected_url, gmaps_url(location)
+		assert_equal expected_url, gmaps_url(FactoryGirl.create(:location))
 	end
 
-	test "transform address string replacing spaces by +" do
+	test "transform string replacing spaces by '+'" do
 		expected = "some+string+without+spaces"
 		assert_equal expected, transform_string("some string without spaces")
+	end
+
+	test "short description with maximum characters size allowed" do
+		maximum_size = 200
+		description = "a" * 210
+		assert_equal maximum_size, short_description(description).size
+	end
+
+	test "short description with suspension points when it's greater than maximum characters size allowed" do
+		description = "a" * 195
+		description += "b" * 5
+		assert_equal true, short_description(description).end_with?("abb...")
+	end
+
+	test "complete description when it's lower than maximum characters size allowed" do
+		description = "just a regular description"
+		assert_equal false, short_description(description).end_with?("...")
 	end
 end
